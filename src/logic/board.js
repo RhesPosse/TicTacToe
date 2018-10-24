63a29fdbc125978cc8eed689b9ecb8903880c57c
Katrín Guðmundsdóttir <katringud17@ru.is>
2018-10-24 16:42:36 +0000

    Board starts with no moves made (#13)

--- src/logic/board.js
+++ src/logic/board.js
@@ -4,6 +4,7 @@ const players = ["X", "O"];
 const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let isWinner = false;
 let isX = true;
+let movesMade = 0;
 
 
 function getPlayers(){
@@ -17,7 +18,18 @@ function getBoard(){
 	return board;
 }
 
-module.exports = {getPlayers, getBoard, isWinner, isX}; 
+function getTotalMoves(){
+
+	return movesMade;
+}
+
+module.exports = {getPlayers, getBoard, isWinner, isX, getTotalMoves}; 
+
+
+
+
+
+
 
 
 
