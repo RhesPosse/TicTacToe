41b68ffdb799d6e9cb4ea2460afceb1b77d4f79d
Katrín Guðmundsdóttir <katringud17@ru.is>
2018-10-24 16:54:27 +0000

    Test refactored after review (#13)

--- src/logic/board.test.js
+++ src/logic/board.test.js
@@ -3,8 +3,7 @@ const board = require("./board");
 
 
 describe('arrayContaining', () => {
-  const expected = ['X', 'O'];
- 
+  
  it('Player can only be X and O', () => {
     expect(['X', 'O']).toEqual(expect.arrayContaining(board.getPlayers()));
   });	
@@ -16,8 +15,7 @@ describe('arrayContaining', () => {
 });
 
 describe('arrayContaining', () => {
-  const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
- 
+  
  it('Board can only be 9 squares', () => {
     expect(board.getBoard()).toHaveLength(9);
   });	
