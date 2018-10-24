1125e468ecdd831bfb0705245d82a45002ea60ef
Katrín Guðmundsdóttir <katringud17@ru.is>
2018-10-24 16:22:44 +0000

    Board created (#13)

--- src/logic/board.js
+++ src/logic/board.js
@@ -1,12 +1,20 @@
 //board.js
 
 const players = ["X", "O"];
+const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function getPlayers(){
 	
 	return players;
 
 }
-module.exports = getPlayers; 
+
+function getBoard(){
+
+	return board;
+}
+
+module.exports = {getPlayers, getBoard}; 
+
 
 
--- src/logic/board.test.js
+++ src/logic/board.test.js
@@ -1,17 +1,27 @@
 //board.test.js
-const getPlayers = require("./board");
+const board = require("./board");
 
 
 describe('arrayContaining', () => {
   const expected = ['X', 'O'];
  
  it('Player can only be X and O', () => {
-    expect(['X', 'O']).toEqual(expect.arrayContaining(getPlayers()));
+    expect(['X', 'O']).toEqual(expect.arrayContaining(board.getPlayers()));
   });	
 
   it('Player can not be other symbols', () => {
-    expect(['X', 'L']).not.toEqual(expect.arrayContaining(getPlayers()));
+    expect(['X', 'L']).not.toEqual(expect.arrayContaining(board.getPlayers()));
   });
+
+});
+
+describe('arrayContaining', () => {
+  const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+ 
+ it('Board can only be 9 squares', () => {
+    expect(board.getBoard()).toHaveLength(9);
+  });	
+
 
 });
 
