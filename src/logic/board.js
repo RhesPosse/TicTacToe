c975b1399030bc7bdd2fcfca14564b9dade2ce77
Katrín Guðmundsdóttir <katringud17@ru.is>
2018-10-24 16:39:54 +0000

    Initialise starting player is X (#13)

--- src/logic/board.js
+++ src/logic/board.js
@@ -3,6 +3,8 @@
 const players = ["X", "O"];
 const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let isWinner = false;
+let isX = true;
+
 
 function getPlayers(){
 	
@@ -15,8 +17,7 @@ function getBoard(){
 	return board;
 }
 
-
-module.exports = {getPlayers, getBoard, isWinner}; 
+module.exports = {getPlayers, getBoard, isWinner, isX}; 
 
 
 
