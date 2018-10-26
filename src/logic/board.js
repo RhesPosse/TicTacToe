968ca154fc08d93f6f91e9ae4945e0289d8b0e6e
Katrín Guðmundsdóttir <katringud17@ru.is>
2018-10-26 13:17:50 +0000

    CheckWin function fixed and added tests (#16)

--- src/logic/board.js
+++ src/logic/board.js
@@ -7,6 +7,14 @@ let isX = true;
 let movesMade = 0;
 
 
+function initializeGame(){
+	board = ["", "", "", "", "", "", "", "", ""];
+	isWinner = false;
+	isX = true;
+	movesMade = 0;
+}
+
+
 function move(move) {
 	if(validateMove(move)) {
 		if(isX) {
@@ -56,14 +64,14 @@ function getTotalMoves(){
  function checkWin() {
 
 	if(
-		(board[0] === board[1] && board[1]=== board[2]) || //rows
-		(board[3] === board[4] && board[4]=== board[5]) ||
-		(board[6] === board[8] && board[7]=== board[8]) ||
-		(board[0] === board[4] && board[4]=== board[8]) ||//diagonal
-		(board[2] === board[4] && board[4]=== board[6]) ||
-		(board[0] === board[3] && board[3]=== board[6]) ||//columns
-		(board[1] === board[4] && board[4]=== board[7]) ||
-		(board[2] === board[5] && board[5]=== board[8]) 
+		((board[0] === board[1] && board[1]=== board[2]) && board[0] !== "") || //rows
+		((board[3] === board[4] && board[4]=== board[5]) && board[3] !== "") ||
+		((board[6] === board[8] && board[7]=== board[8]) && board[6] !== "") ||
+		((board[0] === board[4] && board[4]=== board[8]) && board[0] !== "") ||//diagonal
+		((board[2] === board[4] && board[4]=== board[6]) && board[2] !== "") ||
+		((board[0] === board[3] && board[3]=== board[6]) && board[0] !== "") ||//columns
+		((board[1] === board[4] && board[4]=== board[7]) && board[1] !== "") ||
+		((board[2] === board[5] && board[5]=== board[8]) && board[2] !== "") 
 	){
 		return true;
 	}
@@ -71,7 +79,7 @@ function getTotalMoves(){
 	
 };
 
-module.exports = {getPlayers, getBoard, isWinner, isX, getTotalMoves, move, checkWin}; 
+module.exports = {getPlayers, getBoard, isWinner, isX, getTotalMoves, move, checkWin, initializeGame}; 
 
 
 
