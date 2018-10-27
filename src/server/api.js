b9025130a28342471d66dd7173022bb12a1c9327
Thomas Ari Bech <thomasb17@ru.is>
2018-10-27 18:53:43 +0000

    Fixed that api wasn't calling the function (#25)

--- src/server/api.js
+++ src/server/api.js
@@ -1,8 +1,10 @@
 //api.js
 const express = require("express");
 const router = express.Router();
+const board = require("../logic/board")
 
 router.get("/initializeGame", (request, response) => {
+	board.initializeGame();
 	response.status(200).send({message: "New Game Started"});
 });
 
