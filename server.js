b491afff8848983978fc2a1f4f05eadaf45face4
Thomas Ari Bech <thomasb17@ru.is>
2018-10-27 18:21:37 +0000

    Setup server.js to use the app and listen to a port

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+const app = require("./src/app");
+const port = process.env.PORT || 5000;
+
+app.set("port", port);
+app.listen(port);
+
+console.log(`Server listening on port ${port}`);
