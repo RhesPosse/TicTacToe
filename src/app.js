d700044d266f49bd9f30fd697dcf58ea3b1fda95
Thomas Ari Bech <thomasb17@ru.is>
2018-10-28 15:10:10 +0000

    Connect webpage to app (#25)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 //app.js
 const express = require("express");
+const path = require("path");
 const app = express();
 const api = require("./server/api");
+
+app.use(express.static(path.join(__dirname, "../", "dist")));
 
 app.use("/api", api);
 
