65d81982152013c5d43acb36556d78745f2675cd
Thomas Ari Bech <thomasb17@ru.is>
2018-10-28 19:40:35 +0000

    Made the game now playable (#36)

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,41 @@
-import _ from 'lodash';
+let $ = require("jquery");
 
-function component() {
-	let element = document.createElement('div');
+fetch("./api/initializeGame").then((response) => {
+	response.json().then((data) => { 
+		console.log(data.message);
+	});
+}).catch((err) => {
+	console.log("oof");
+});
 
-	element.innerHTML = _.join(['Hello', 'there!'], ' ');
+$("td").each(function() {
+	$(this).on("click", function() {
+		move($(this));
+	});
+});
 
-	return element;
+function move(element) {
+	fetch("./api/move/" + element.attr("id")).then((response) => {
+		response.json().then((data) => { 
+			console.log(data.message);
+			if (data.updatedSquare !== "") {
+				element.text(data.updatedSquare);
+			}
+			checkGameOver();
+		});
+	}).catch((err) => {
+		console.log("oof");
+	});
+}
 
-};
-
-document.body.appendChild(component());
+function checkGameOver() {
+	fetch("./api/gameState").then((response) => {
+		response.json().then((data) => { 
+			if (data.state !== "Unfinished") {
+				console.log(data.state)
+			}
+		});
+	}).catch((err) => {
+		console.log("oof");
+	});
+}
