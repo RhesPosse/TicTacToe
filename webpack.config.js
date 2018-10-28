3c197bf596db338ed71d3682a7c14ca2dba7411f
Karl Arnar Ægisson <karla16@ru.is>
2018-10-28 13:04:08 +0000

    added html webpack plugin and clean webpack plugin and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: './src/client/index.js',
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Tic Tac Toe'
+		}),
+		new CleanWebpackPlugin([
+			'dist'
+		])
+	],
+	devServer: {
+		port: 3000,
+		open: true,
+		proxy: {
+			"/api": "http://localhost:5000"
+		}
 	}
 }
