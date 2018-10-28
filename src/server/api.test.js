1eb6fb2aff56df5b8c42706823baa36ed71ae574
Thomas Ari Bech <thomasb17@ru.is>
2018-10-28 13:21:22 +0000

    Added api call to move (#25)

--- src/server/api.test.js
+++ src/server/api.test.js
@@ -79,6 +79,17 @@ describe("GET printBoard", () => {
 	});
 });
 
+describe("GET move", () => {
+	it("should return a 200 OK status code", async () => {
+		const response = await request(app).get("/api/move/1");
+		expect(response.status).toBe(200);  
+	});
+	it("should return message in a object", async () => {
+		const response = await request(app).get("/api/move/1");
+		expect(response.body).toHaveProperty("message");
+	});
+});
+
 describe("GET gameState", () => {
 	it("should return a 200 OK status code", async () => {
 		const response = await request(app).get("/api/gameState");
