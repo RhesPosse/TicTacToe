161f24766f40dab4d24bbb64909d0e41e3d04722
Karl Arnar Ægisson <karla16@ru.is>
2018-10-28 21:55:45 +0000

    pdf are stored in circleci

--- docs/pdfConvert.js
+++ docs/pdfConvert.js
@@ -1,13 +1,13 @@
 const markdownpdf = require("markdown-pdf");
 
-markdownpdf().from("./AdministrationManual.md").to("./PDF/AdministrationManual.pdf", function () {
+markdownpdf().from("./docs/AdministrationManual.md").to("./docs/PDF/AdministrationManual.pdf", function () {
 	console.log("Administration Manual to pdf done");
 });
 
-markdownpdf().from("./DesignReport.md").to("./PDF/DesignReport.pdf", function () {
+markdownpdf().from("./docs/DesignReport.md").to("./docs/PDF/DesignReport.pdf", function () {
 	console.log("Design Report to pdf done");
 });
 
-markdownpdf().from("./DevelopmentManual.md").to("./PDF/DevelopmentManual.pdf", function () {
+markdownpdf().from("./docs/DevelopmentManual.md").to("./docs/PDF/DevelopmentManual.pdf", function () {
 	console.log("Development Manual to pdf done");
 });
